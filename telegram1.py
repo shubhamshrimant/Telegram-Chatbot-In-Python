5366e7ae87d51ad63b17f32af1b04aae5721285b
Shubham Shrimant <32372277+shubhamshrimant@users.noreply.github.com>
2021-05-27 15:23:54 +0530

    Update telegram1.py

--- telegram1.py
+++ telegram1.py
@@ -67,5 +67,6 @@ def index():
 
 
 if __name__ == '__main__':
-   port = int(os.environ.get("PORT", 33507))
-   app.run(host='0.0.0.0',port=port,threaded=True)
+   #port = int(os.environ.get("PORT", 33507))
+   #app.run(host='0.0.0.0',port=port,threaded=True)
+   app.run(debug=True)
