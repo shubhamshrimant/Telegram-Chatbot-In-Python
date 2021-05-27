096e872cbd68b0f122b7a7464f4b62e9b8367cdf
Shubham Shrimant <32372277+shubhamshrimant@users.noreply.github.com>
2021-05-27 10:16:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from os import environ
+#from os import environ
 from flask import Flask
 
 app = Flask(__name__)
-app.run(environ.get('PORT'))
+#app.run(environ.get('PORT'))
+if __name__ == '__main__':
+    app.run()
